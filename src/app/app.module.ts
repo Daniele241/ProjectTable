321d23ce72abd801ad7312d1e8c74414c739d45d
Daniele <daniele@MacBook-Pro-di-Daniele.local>
2018-10-26 09:33:22 +0200

    modifica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material';
+import { HeaderComponent } from './header/header.component';
+import { TableComponent } from './table/table.component';
+import { ListTableComponent } from './table/list-table/list-table.component';
+import { ItemTableComponent } from './table/item-table/item-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableComponent,
+    ListTableComponent,
+    ItemTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.ts
@@ -0,0 +1,30 @@
+import {
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatIconModule
+} from '@angular/material';
+
+import { NgModule } from '@angular/core';
+@NgModule({
+imports: [
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatIconModule
+],
+exports: [
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatIconModule
+],
+})
+export class MaterialModule {
+}
