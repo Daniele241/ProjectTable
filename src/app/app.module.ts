353aefa0d4ca5120c25f6637ddf19ef797e09945
lore14 <lory.vene@gmail.com>
2018-10-26 14:34:43 +0200

    eliminato componente header + gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material';
-import { HeaderComponent } from './header/header.component';
 import { TableComponent } from './table/table.component';
 import { ListTableComponent } from './table/list-table/list-table.component';
 import { ItemTableComponent } from './table/item-table/item-table.component';
@@ -14,7 +13,6 @@ import { ItemTableComponent } from './table/item-table/item-table.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     TableComponent,
     ListTableComponent,
     ItemTableComponent
