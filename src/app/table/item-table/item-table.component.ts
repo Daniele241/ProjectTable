0b16d06c210037838604b185f796cd34667a2f86
Daniele <dellacquadaniele97@gmail.com>
2018-10-29 21:49:10 +0100

    fix delete row

--- src/app/table/item-table/item-table.component.ts
+++ src/app/table/item-table/item-table.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { TableModel } from '../../../model/table.model';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
+import { Headers } from '@angular/http';
 
 import 'rxjs/add/operator/map';
-import 'rxjs/RX'
+import 'rxjs/RX';
+
+let headers = new Headers({ 'Content-Type': 'application/json' });
+//let options = new RequestOptions({ headers: headers });
 
 @Component({
   selector: 'app-item-table',
@@ -24,8 +28,7 @@ export class ItemTableComponent implements OnInit {
     'edit',
     'delete'
   ];
-  arrTableItem: TableModel[] = [];
-  
+
   dataSource = new MatTableDataSource<TableModel>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -36,30 +39,45 @@ export class ItemTableComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  deleteItem(id: TableModel): void {
+    this.httpClient.delete("http://localhost:3000/items/" + id.id)
+    .subscribe(
+      (res: any[]) => {
+        //this.dataSource.data.splice(id.id, 1);
+        this.getItem();
+      }
+    )    
+  }
+
   getItem() {
-    this.httpClient.get('http://localhost:3000/items') 
+    this.httpClient.get('http://localhost:3000/items')
+    //this.httpClient.get('http://localhost:60634/api/competitor') 
     .map((response: any) => {
 
     const res = response;
     const result : TableModel[] = [];
 
     for(let item of res) {
-    const currentItem = new TableModel(
-      item.plant, 
-      item.competitorCode,
-      item.competitor,
-      item.productCode,
-      item.product,
-      item.Date,
-      item.document,
+      const currentItem = new TableModel(
+        item.id,
+        item.plant, 
+        item.competitorCode,
+        item.competitor,
+        item.productCode,
+        item.product,
+        item.Date,
+        item.document,
+      );
+      result.push(currentItem); 
+    } 
+    return result; 
+    }) 
+    .subscribe(
+      (result: TableModel[]) => {
+        this.dataSource.data = result; 
+        console.log(this.dataSource); 
+      } 
     );
-    result.push(currentItem); 
-  } 
-  return result; }) .subscribe(
-  (result: TableModel[]) => {
-  this.dataSource.data = result; 
-  console.log(this.dataSource); 
-} );
   } 
 }
 
